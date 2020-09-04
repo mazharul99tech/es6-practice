e4e8d9dab7685751f1317120153e4297445db2f9
mazharul99tech <mazharul99tech@gmail.com>
2020-09-04 22:00:25 +0600

    default added

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const hubby = "Elias Kanchan";
+console.log(hubby);
+
+const numbers = [12, 14, 45];
+numbers[1] = 30;
+numbers.push(50);              
+const nayok = {name:"Sakib Khan", phone: 98777};
+// console.log(nayok);
+// console.log(numbers);
+
+let patientName = "Rahim Chacha";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum = 0;
+for(let i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
