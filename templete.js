1d19aa79d150b2d45013d29b1222c2289a24d9b7
mazharul99tech <mazharul99tech@gmail.com>
2020-09-05 01:01:32 +0600

    array function added

---
+++ templete.js
@@ -0,0 +1,14 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${20+50+30} is a good boy.`;
+
+const multiline = "I love you\n"
+    + "I miss you\n"
+    + "I need you";
+
+const multiline2 = `I love you
+I miss you
+No. I don't need you
+Bailly road a dorker nai`;    
+console.log(multiline2);
